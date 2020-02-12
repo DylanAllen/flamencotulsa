0ab2f11f53154c2e399d95c0d5e488ac6ccf8deb
Dylan <dylan.allen@voicefoundry.com>
2020-02-12 13:20:00 -0600

    Revert "added posy product to yaml"
    This reverts commit d3412b0afbcd5518b6bb5df214c5f1de8c04de7f.

--- lambdas/products.js
+++ lambdas/products.js
@@ -14,20 +14,6 @@ const getProducts = async () => {
   }
 }
 
-const postProduct = async (product) => {
-  const params = {
-    TableName: process.env["PRODUCT_TABLE"],
-    Item: product
-  }
-  try {
-    const resp = await ddb.put(params).promise();
-    return apiResponse(resp)
-  } catch(err) {
-    console.log('put error', err);
-    return apiResponse(null);
-  }
-}
-
 const apiResponse = (data) => {
   return  {
     statusCode: 200,
@@ -44,7 +30,7 @@ module.exports.handler = async (event, context) => {
     case 'GET':
       return await getProducts();
     case 'POST':
-      return await postProduct(JSON.parse(event.body));
+      return await getProducts();
     default:
       return apiResponse({
         message: 'invalid input'
